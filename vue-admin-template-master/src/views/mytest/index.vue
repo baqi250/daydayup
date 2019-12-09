e41b4dd8f9fc0402458257e2b0bb3e94387523ae
gjq <baqi250@163.com>
2019-12-09 16:57:48 +0800

    ss
    dsd

---
+++ vue-admin-template-master/src/views/mytest/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">mytest</div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import axios from 'axios'
+
+export default {
+  name: 'Mytest',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  },
+  created(){
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
